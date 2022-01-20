3a08b6ad50e4d4884750b6f9376f01b6b1f6c988
wiily <willyhuang18@gmail.com>
2022-01-19 21:16:45 -0800

    set attribute for each img

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 //declare a var for img source
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 //declare starting img with the query selector
+var image1 = document.querySelectorAll("img")[0];
+//giving attribute for every img
+image1.setAttribute("src", randomImageSource);
